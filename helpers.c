5da84770b50f1df4f28a60096f3c42123cb8de93
Omar Sadek <sadekm.omar@gmail.com>
2022-11-21 15:26:01 +0200

    Edge detection

--- helpers.c
+++ helpers.c
@@ -65,13 +65,65 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = avgRed;
         }
     }
-
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    int Gx[3][3] = {{-1,0,1}, {-2,0,2}, {-1,0,1}};
+    int Gy[3][3] = {{-1,-2-1}, {0,0,0}, {1,2,1}};
     
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumBlueGx = 0, sumGreenGx = 0, sumRedGx = 0, sumBlueGy = 0, sumGreenGy = 0, sumRedGy = 0;
+            int rowCounter = 0, colCounter = 0;
+
+            // Selecting the nine pixels and taking a weighted sum.
+            for (int row = -1; row < 2; row++)
+            {
+                for (int col = -1; col < 2; col++)
+                {
+                    if ((((i + row) < 0) | ((j + col) < 0) | ((i + row) == height) | ((j + col) == width)) == 0)
+                    {   
+                        // Convolution & Sum of Gx
+                        int blueGx = image[i + row][j + col].rgbtBlue * Gx[rowCounter][colCounter];
+                        int greenGx = image[i + row][j + col].rgbtGreen * Gx[rowCounter][colCounter];
+                        int redGx = image[i + row][j + col].rgbtRed * Gx[rowCounter][colCounter];
+
+                        sumBlueGx += blueGx;
+                        sumGreenGx += greenGx;
+                        sumRedGx += redGx;
+
+                        // Convolution & Sum of Gy
+                        int blueGy = image[i + row][j + col].rgbtBlue * Gy[rowCounter][colCounter];
+                        int greenGy = image[i + row][j + col].rgbtGreen * Gy[rowCounter][colCounter];
+                        int redGy = image[i + row][j + col].rgbtRed * Gy[rowCounter][colCounter];
+
+                        sumBlueGy += blueGy;
+                        sumGreenGy += greenGy;
+                        sumRedGy += redGy;
+                    }
+                    colCounter++;
+                }
+                rowCounter++;
+            }
+
+            double sumBlue = sqrt( pow((double)sumBlueGx, 2) + pow((double)sumBlueGy, 2) );
+            double sumGreen = sqrt( pow((double)sumGreenGx, 2) + pow((double)sumGreenGy, 2) );
+            double sumRed = sqrt( pow((double)sumRedGx, 2) + pow((double)sumRedGy, 2) );
+
+            int sumIntBlue = (int)sumBlue % 256;
+            int sumIntGreen = (int)sumGreen % 256;
+            int sumIntRed = (int)sumRed % 256;
+
+            image[i][j].rgbtBlue = sumBlue;
+            image[i][j].rgbtGreen = sumGreen;
+            image[i][j].rgbtRed = sumRed;
+            
+        }
+    }
     return;
 }
